925d8380b759bc1aac57475803b89fdb3d3e6fcd
lol13371 <117176678+lol13371@users.noreply.github.com>
2023-02-13 06:48:19 +0600

    Update main.b607e494.chunk.js

--- static/js/main.b607e494.chunk.js
+++ static/js/main.b607e494.chunk.js
@@ -663,7 +663,7 @@
                                                 children: " (excluding gas fees) "
                                             }), Object(N.jsx)("h2", {
                                                 className: "mint-limited text-shadow",
-                                                children: "LIMITED SALE"
+                                                children: "LIMITED SUPERBOWL SALE"
                                             })]
                                         }), Object(N.jsxs)("h3", {
                                             className: "mint-count",
